3ec57411a9f99d28a05bc992b3535772b631c83e
Paaatss <pat.sr96@gmail.com>
2024-08-28 02:57:45 -0600

    PRIMERA VERSIÓN

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function ShowHide(id) {
+    const divContainer = document.getElementById(id);
+    if (divContainer.style.display === 'none' || divContainer.style.display === '') {
+        divContainer.style.display = 'block';
+    } else {
+        divContainer.style.display = 'none';
+    }
+}
