147db67af6b8da23fbd31c7d68bc50191564af32
Paaatss <pat.sr96@gmail.com>
2024-08-28 22:40:57 -0600

    cORRECCION DE FORMULARIO JS

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function ShowHide(id) {
         divContainer.style.display = 'none';
     }
 }
+
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_nmeonha';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
