1ae4deb8057337b9cf86b05a16c503e1aa6c3a90
Paaatss <pat.sr96@gmail.com>
2024-08-29 19:15:36 -0600

    animaciones

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 
 
 function ShowHide(id) {
-    const divContainer = document.getElementById(id);
-    if (divContainer.style.display === 'none' || divContainer.style.display === '') {
-        divContainer.style.display = 'block';
-    } else {
-        divContainer.style.display = 'none';
-    }
+  const divContainer = document.getElementById(id);
+  if (divContainer.style.display === 'none' || divContainer.style.display === '') {
+    divContainer.style.display = 'block';
+  } else {
+    divContainer.style.display = 'none';
+  }
 }
 
 
 const btn = document.getElementById('button');
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
 
-   btn.value = 'Sending...';
+    btn.value = 'Sending...';
 
-   const serviceID = 'default_service';
-   const templateID = 'template_nmeonha';
+    const serviceID = 'default_service';
+    const templateID = 'template_nmeonha';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
